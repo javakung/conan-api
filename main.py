4fe04ac89a8eadd9aa70d8608dbd6ff12689c495
Phogsiri309 <phogsiri-st@rmutsb.ac.th>
2020-11-30 14:08:36 +0700

    Normalize_Number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Author: Chatchawal Sangkeettrakarn
-#Date: September 20,2020.
+# Author: Chatchawal Sangkeettrakarn
+# Date: September 20,2020.
 
 from fastapi import FastAPI
 import uvicorn
@@ -12,28 +12,34 @@ from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
+
 def result(res):
-    return {"result":res}
+    return {"result": res}
+
 
 @app.get("/")
 async def main():
     return 'Hello World'
 
+
 @app.get("/test")
 async def test():
     return 'Test Tutorial'
+
 
 @app.get("/add")
 async def add(a: int = 0, b: int = 0):
     return a+b
 
+
 @app.get("/mul")
 async def mul(a: int = 0, b: int = 0):
     return a*b
 
+
 @app.get("/pow")
 async def pow(a: int = 0, b: int = 0):
-    return math.pow(a,b)
+    return math.pow(a, b)
 
 
 def tonumlist(li):
@@ -42,11 +48,13 @@ def tonumlist(li):
         ls[i] = float(ls[i])
     return list(ls)
 
+
 @app.get("/asc")
 async def asc(li):
     ls = tonumlist(li)
     ls.sort()
     return ls
+
 
 @app.get("/desc")
 async def desc(li):
@@ -54,58 +62,101 @@ async def desc(li):
     ls.sort(reverse=True)
     return ls
 
+
 @app.get("/sum")
 async def sum(li):
     ls = tonumlist(li)
     return np.sum(np.array(ls))
+
 
 @app.get("/avg")
 async def avg(li):
     ls = tonumlist(li)
     return np.average(ls)
 
+
 @app.get("/mean")
 async def mean(li):
     ls = tonumlist(li)
     return np.mean(ls)
+
 
 @app.get("/max")
 async def max(li):
     ls = tonumlist(li)
     return np.amax(ls)
 
+
 @app.get("/min")
 async def min(li):
     ls = tonumlist(li)
     return np.amin(ls)
 
+
+@app.get("/NormalizeNumber")
+async def number(Number: str = ""):
+    if '.' in Number:
+        nsplit = Number.split('.')
+        if (',' in nsplit[0]):
+            n2split = nsplit[0].split(',')
+            for ch in n2split[1::]:
+                if len(ch) == 3:
+                    a1 = float(Number.replace(',', ''))
+                    status = 'True'
+                else:
+                    a1 = Number
+                    status = 'False'
+        else:
+            a1 = float(Number.replace(',', ''))
+            status = 'True'
+    elif len(Number) == 3:
+        a1 = float(Number.replace(',', ''))
+        status = 'True'
+    elif ',' in Number:
+        Csplit = Number.split(',')
+        for b in Csplit[1::]:
+            if len(b) == 3:
+                a1 = float(Number.replace(',', ''))
+                status = 'True'
+            else:
+                a1 = Number
+                status = 'False'
+    else:
+        a1 = Number
+        status = 'False'
+
+    jsonout = {'a1': a1, 'status': status}
+    return jsonout
+
+
 @app.get("/validation-ctzid")
 async def validation_ctzid(text):
     if(len(text) != 13):
         return False
-    
+
     sum = 0
     listdata = list(text)
-    
-    for i in range(12):
-        sum+=int(listdata[i])*(13-i)
 
-    d13 = (11-(sum%11))%10
-        
-    return d13==int(listdata[12])
+    for i in range(12):
+        sum += int(listdata[i])*(13-i)
+
+    d13 = (11-(sum % 11)) % 10
+
+    return d13 == int(listdata[12])
+
 
 @app.get("/validation-email")
-async def validation_email(text):  
+async def validation_email(text):
     regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-    if re.search(regex,text):
+    if re.search(regex, text):
         return True
     else:
         return False
-    
-    
-@app.get("/google-search",response_class=PlainTextResponse)
+
+
+@app.get("/google-search", response_class=PlainTextResponse)
 def google_search(text):
-    
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -116,10 +167,10 @@ def google_search(text):
         'Upgrade-Insecure-Requests': '1'
     }
     url = 'https://www.google.com/search?q=' + str(text)
-    res = requests.get(url, headers = headers)
+    res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.content, 'html.parser')
-    
-    t = soup.findAll('div', {'class':"r"})
+
+    t = soup.findAll('div', {'class': "r"})
     i = 0
     result = ''
     for a in t:
@@ -129,11 +180,9 @@ def google_search(text):
         i += 1
         if(i >= 5):
             break
-    
+
     return(result)
 
 
-
-
 if __name__ == '__main__':
-   uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
+    uvicorn.run(app, host="0.0.0.0", port=80, debug=True)
